93ad7bf2b4e490f7fca71c650d8e1c3d816dd6ae
Ranjit singh <jiradhey402@gmail.com>
2023-03-06 15:12:39 +0530

    data dumped successfully in the mongodb

--- data_dump.py
+++ data_dump.py
@@ -1,9 +1,13 @@
 import pymongo
 import pandas as pd
 import json
-client = pymongo.MongoClient("mongodb://localhost:27017/neurolabDB")
+import os
+from sensor.config import mongo_client
 
-DATA_FILE_PATH = "/config/workspace/aps_failure_training_set1.csv"
+# mongodb_url = os.getenv('MONGO_DB_URL')
+# client = pymongo.MongoClient(mongodb_url)
+
+DATA_FILE_PATH = (r"C:\Users\Ranjit Singh\Desktop\Aps-sensor-fault-detection\aps_failure_training_set1.csv")
 DATABASE_NAME = "aps"
 COLLECTION_NAME = "sensor"
 
@@ -15,5 +19,5 @@ if __name__=="__main__":
 
     json_record = list(json.loads(df.T.to_json()).values())
     print(json_record[0])
-
-    client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
+    mongo_client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
+    print("inserted documented :- ",mongo_client[DATABASE_NAME][COLLECTION_NAME].count_documents())
--- sensor/config.py
+++ sensor/config.py
@@ -7,9 +7,10 @@ import os
 @dataclass
 class EnvironmentVariable:
     mongo_db_url:str = os.getenv("MONGO_DB_URL")
+    target_column:str = os.getenv('TARGET_COLUMN')
     aws_acess_key_id:str = os.getenv("AWS_ACCESS_KEY_ID")
     aws_secret_acess_key:str = os.getenv("AWS_SECRET_ACESS_KEY")
 
 env_var = EnvironmentVariable()
 mongo_client = pymongo.MongoClient(env_var.mongo_db_url)
-TARGET_COLUMN="class"
+TARGET_COLUMN= env_var.target_column
